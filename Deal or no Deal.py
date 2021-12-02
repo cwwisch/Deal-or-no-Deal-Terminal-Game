335fbae86b576099d9f2460e11011245474fbaff
cwwisch <94135994+cwwisch@users.noreply.github.com>
2021-12-02 02:09:00 -0600

    Add files via upload
    added comments for a possible confusion area.

--- Deal or no Deal.py
+++ Deal or no Deal.py
@@ -1,5 +1,7 @@
 import random
+# this list is used for making each case have a random value that isn't repeated.
 list_options = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000]
+# this list, while exactly the same as the previous one, is used for a seperate purpose of notifying the player of what money values are still in play.
 values_in_play = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750, 1000, 5000, 10000, 25000, 50000, 75000, 100000, 200000, 300000, 400000, 500000, 750000, 1000000]
 cases = {}
 
